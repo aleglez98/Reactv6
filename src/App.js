0ea4153205dead7d849cc4baf1027d69ee10a31d
Alejandro González González <agongon@arte-consultores.com>
2021-11-19 21:37:22 +0000

    Javascript tools complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-const Pet = (props) => {
-    return React.createElement("div",{}, [
-        React.createElement("h2", {}, props.name),
-        React.createElement("h3", {}, props.animal),
-        React.createElement("h3", {}, props.breed),
-    ]
-
-    )
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pet from './Pet';
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1",{},"Adopt Me!"),
-            React.createElement(Pet, { name: "Budy", animal: "Dog", breed: "Pequines"}),
-            React.createElement(Pet, { name: "Pepper", animal: "Cat", breed: "Ratata"}),
-            React.createElement(Pet, { name: "pikachu", animal:"bird", breed: "Loro"}),
-        ]
-        
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "my-band" }, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Budy",
+      animal: "Dog",
+      breed: "Pequines",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Cat",
+      breed: "Ratata",
+    }),
+    React.createElement(Pet, {
+      name: "pikachu",
+      animal: "bird",
+      breed: "Loro",
+    }),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
