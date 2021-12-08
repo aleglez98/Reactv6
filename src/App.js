233dd88cd642f044abe080282672a9983ad03ba4
Alejandro González González <agongon@arte-consultores.com>
2021-12-08 16:53:33 +0000

    Finalized Chapter 3: Core React Concepts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pet from './Pet';
+import SearchParams from './SearchParams';
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-band" }, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Budy",
-      animal: "Dog",
-      breed: "Pequines",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Cat",
-      breed: "Ratata",
-    }),
-    React.createElement(Pet, {
-      name: "pikachu",
-      animal: "bird",
-      breed: "Loro",
-    }),
-  ]);
-};
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", { id: "my-band" }, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Budy",
+//       animal: "Dog",
+//       breed: "Pequines",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Pepper",
+//       animal: "Cat",
+//       breed: "Ratata",
+//     }),
+//     React.createElement(Pet, {
+//       name: "pikachu",
+//       animal: "bird",
+//       breed: "Loro",
+//     }),
+//   ]);
+// };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+const App = () => (
+  <div>
+    <h1 id="my-band">Adopt Me!</h1>
+    <SearchParams></SearchParams>
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById("root"));
